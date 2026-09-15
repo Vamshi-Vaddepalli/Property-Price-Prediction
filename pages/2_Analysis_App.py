680607d0db90567e82ccb160111f48ed0e2381f6
Vamshi Vaddepalli <vamshivaddepelli@gmail.com>
2026-09-16 01:00:52 +0530

    Removed map_style because this uupdate was not needed

--- pages/2_Analysis_App.py
+++ pages/2_Analysis_App.py
@@ -26,8 +26,7 @@ fig = px.scatter_map(group_df, lat="latitude", lon="longitude", color="price_per
                             'built_up_area': True,
                             'latitude': False,
                             'longitude': False
-                        },
-                    map_style="open-street-map"       
+                        }                  
                   )
 
 st.plotly_chart(fig,width='stretch')
@@ -121,8 +120,7 @@ fig = px.scatter_map(group2_df, lat='latitude', lon='longitude', size='property_
         'latitude': False,
         'longitude': False
     },
-    color_continuous_scale='electric', zoom=10, width=1200, height=700, size_max=30,
-    map_style="open-street-map"
+    color_continuous_scale='electric', zoom=10, width=1200, height=700, size_max=30
 )
 
 st.plotly_chart(fig)
